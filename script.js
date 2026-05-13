c7dbc09769d6912a003b01b037c84ba6b71619a5
Dylancooi <162858697+Dylancooi@users.noreply.github.com>
2026-05-12 21:44:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -36,3 +36,73 @@ window.onclick = (e) => {
     popup.style.display = "none";
   }
 };
+
+// =========================
+// TIMER 1
+// =========================
+
+const countdown1 =
+new Date("May 20, 2026 00:00:00").getTime();
+
+// =========================
+// TIMER 2
+// =========================
+
+const countdown2 =
+new Date("June 1, 2026 00:00:00").getTime();
+
+// =========================
+// UPDATE TIMERS
+// =========================
+
+setInterval(() => {
+
+    const now = new Date().getTime();
+
+    // TIMER 1
+    const distance1 = countdown1 - now;
+
+    const days1 =
+    Math.floor(distance1 / (1000 * 60 * 60 * 24));
+
+    const hours1 =
+    Math.floor((distance1 % (1000 * 60 * 60 * 24))
+    / (1000 * 60 * 60));
+
+    const minutes1 =
+    Math.floor((distance1 % (1000 * 60 * 60))
+    / (1000 * 60));
+
+    const seconds1 =
+    Math.floor((distance1 % (1000 * 60))
+    / 1000);
+
+    document.getElementById("days1").innerHTML = days1;
+    document.getElementById("hours1").innerHTML = hours1;
+    document.getElementById("minutes1").innerHTML = minutes1;
+    document.getElementById("seconds1").innerHTML = seconds1;
+
+    // TIMER 2
+    const distance2 = countdown2 - now;
+
+    const days2 =
+    Math.floor(distance2 / (1000 * 60 * 60 * 24));
+
+    const hours2 =
+    Math.floor((distance2 % (1000 * 60 * 60 * 24))
+    / (1000 * 60 * 60));
+
+    const minutes2 =
+    Math.floor((distance2 % (1000 * 60 * 60))
+    / (1000 * 60));
+
+    const seconds2 =
+    Math.floor((distance2 % (1000 * 60))
+    / 1000);
+
+    document.getElementById("days2").innerHTML = days2;
+    document.getElementById("hours2").innerHTML = hours2;
+    document.getElementById("minutes2").innerHTML = minutes2;
+    document.getElementById("seconds2").innerHTML = seconds2;
+
+}, 1000);
